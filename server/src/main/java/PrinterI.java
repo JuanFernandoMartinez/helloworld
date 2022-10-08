d1f3a4b6a3c28c3ef814a837da9caf695dceb484
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2022-10-07 19:00:34 -0500

    Added execution threadpool

--- server/src/main/java/PrinterI.java
+++ server/src/main/java/PrinterI.java
@@ -1,5 +1,8 @@
 
 import java.math.BigInteger;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class PrinterI implements Demo.Printer
 {
     public String printString(String s, com.zeroc.Ice.Current current)
@@ -7,34 +10,27 @@ public class PrinterI implements Demo.Printer
         String prt[] = s.split(" ");
         String hn = prt[0];
         String num = prt[1];
+        ThreadPoolExecutor executor =
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(6);
         if (num.matches("[-]{0,1}[0-9]+")){
             long n = Long.parseLong(num);
             if (n>0) {
+                executor.submit(()->{
                 System.out.print(hn+": ");
                 return fibonacci(n);
-            }
-            else System.out.println(s);
+            });
+            }else System.out.println(s);
         }else System.out.println(s);
-
         return "0";
-        
     }
 
     public String fibonacci(long nthNumber) {
-        //use loop
-
-
         BigInteger previouspreviousNumber, previousNumber = BigInteger.valueOf(0), currentNumber = BigInteger.valueOf(1);
-
         for (int i = 1;   i < nthNumber ; i++) {
-
             previouspreviousNumber =  previousNumber ;
-
             previousNumber =  currentNumber ;
-
             currentNumber = previouspreviousNumber.add( previousNumber);
             System.out.print(currentNumber.toString()+" ");
-
         }
         return currentNumber.toString();
     }
